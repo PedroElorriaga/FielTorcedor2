65d27b0bc4a48fb71596d2702bc1f0fc4ae4f35d
PedroElorriaga <pedrohes2002@outlook.com>
2022-10-23 22:09:39 -0300

    funcao-onScream

--- position.py
+++ position.py
@@ -1,4 +1,23 @@
+from http import server
 import pyautogui
+from time import sleep
 
-x, y = pyautogui.locateCenterOnScreen('img/Pesquisa.png')
-pyautogui.click(x, y)
+
+def search_url(url):
+    search = True
+    while search:
+        sleep(0.5)
+        try:
+            x, y = pyautogui.locateCenterOnScreen('img/Pesquisa.png')
+            pyautogui.moveTo(x, y)
+            pyautogui.PAUSE = 1
+            pyautogui.click()
+            pyautogui.PAUSE = 0.5, pyautogui.write(
+                url), pyautogui.press('enter')
+            search = False
+        except:
+            pyautogui.alert(text='Não encontrado')
+
+
+inputUrl = pyautogui.password(text='Digite a URL', title='URL', mask='')
+search_url(inputUrl)
