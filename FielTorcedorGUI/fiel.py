bf092b555f16fa1e91b2c50a9aadd4056cf0e368
PedroElorriaga <pedrohes2002@outlook.com>
2022-10-24 22:27:18 -0300

    feito-login

--- FielTorcedorGUI/fiel.py
+++ FielTorcedorGUI/fiel.py
@@ -2,9 +2,11 @@
 from asyncio.windows_events import NULL
 from cgitb import text
 import imp
+import importlib
 from turtle import title
 import pyautogui
 from time import sleep
+import re
 
 """while True:
     sleep(2)
@@ -29,32 +31,77 @@ while userInfo:
         userInfo = False
 
 pyautogui.alert(
-    text='Verifique se o  CAPS LOCK está desativado, depois clique em confirmar', button='Confirmar')
-# Buscar o chrome pela aba de pesquisas
+    text='Verifique se o  CAPS LOCK está desativado, depois clique em confirmar',
+    button='Confirmar')
+
+# Buscar o chrome pela aba de pesquisas e abrir o chrome
 pyautogui.press('win')
 pyautogui.PAUSE = 2
 pyautogui.write('chrome')
 pyautogui.PAUSE = 1
-# Abrir o chrome
 pyautogui.press('enter')
 pyautogui.PAUSE = 1
+
 # Digitar a URL do fielTorecedor
-pyautogui.click(x=611, y=75)
 pyautogui.PAUSE = 0.5
 pyautogui.write('fieltorcedor.com.br')
 pyautogui.press('enter')
+
+
 # Fechar anuncio inicial
-sleep(2)
-pyautogui.click(x=1848, y=200)
+def closeAd():
+    tentativas = True
+    while tentativas:
+        pyautogui.click(x=1421, y=619)
+        try:
+            x, y = pyautogui.locateCenterOnScreen('img/closeads.png')
+            pyautogui.moveTo(x, y)
+            pyautogui.PAUSE = 0.5, pyautogui.click()
+            tentativas = False
+        except:
+            pyautogui.PAUSE = 0.5
+            print('CloseAdALERT')
+
+
+closeAd()
+
+
 # Entrar na tela de login
-pyautogui.click(x=1665, y=248)
+def enterLog():
+    pyautogui.PAUSE = 0.5
+    tentativas = True
+    while tentativas:
+        try:
+            locate = pyautogui.locateCenterOnScreen('img/enterbutton.png')
+            pyautogui.click(locate.x, locate.y)
+            tentativas = False
+        except:
+            pyautogui.PAUSE = 0.2
+
+
+enterLog()
+
+
 # Digitar as credenciais e passar pelo reCaptcha
-pyautogui.click(x=1010, y=427)
-pyautogui.write(getUserId)
-pyautogui.PAUSE = 0.5
-pyautogui.click(x=1360, y=545)
-pyautogui.write(getUserPass)
-pyautogui.click(x=793, y=665)
-sleep(1.5)
-pyautogui.click(x=1421, y=619)
-#
+def credentialToken(userId, passw):
+    tentativas = True
+    while tentativas:
+        try:
+            user = pyautogui.locateCenterOnScreen(
+                'img/Userid.png', confidence=0.4)
+            pyautogui.moveTo(user.x, user.y), pyautogui.click(
+            ), pyautogui.write(userId)
+            password = pyautogui.locateCenterOnScreen(
+                'img/Passwo.png', confidence=0.4)
+            pyautogui.moveTo(password.x, password.y), pyautogui.click()
+            pyautogui.write(passw)
+            recaptcha = pyautogui.locateCenterOnScreen('img/Recapcha.png')
+            pyautogui.click(recaptcha.x, recaptcha.y), sleep(0.3)
+            enter = pyautogui.locateCenterOnScreen('img/login.png')
+            pyautogui.click(enter.x, enter.y)
+            tentativas = False
+        except:
+            print('TokenALERT')
+
+
+credentialToken(getUserId, getUserPass)
--- position.py
+++ position.py
@@ -3,21 +3,11 @@ import pyautogui
 from time import sleep
 
 
-def search_url(url):
-    search = True
-    while search:
-        sleep(0.5)
-        try:
-            x, y = pyautogui.locateCenterOnScreen('img/Pesquisa.png')
-            pyautogui.moveTo(x, y)
-            pyautogui.PAUSE = 1
-            pyautogui.click()
-            pyautogui.PAUSE = 0.5, pyautogui.write(
-                url), pyautogui.press('enter')
-            search = False
-        except:
-            pyautogui.alert(text='Não encontrado')
+buttond = pyautogui.click(x=892, y=296)
 
+'''button7location = pyautogui.locateOnScreen('img/Userid.png')
+print(button7location)
 
-inputUrl = pyautogui.password(text='Digite a URL', title='URL', mask='')
-search_url(inputUrl)
+button7location = pyautogui.center(button7location)
+print(button7location)
+'''
